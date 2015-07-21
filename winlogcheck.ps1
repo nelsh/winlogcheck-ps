862c744bc85cc55fc773dbbcf9e375f05490e4f4
nelsh <env@virtech.ru>
2015-07-21 13:06:16 +0800

    Logging and refactoring

--- winlogcheck.ps1
+++ winlogcheck.ps1
@@ -3,11 +3,11 @@ Param($mode,$log,$filter)
 
 # Print usage information
 function writeUsage($msg) {
-    Write-Host "$msg`n" -foregroundcolor "red"
+    LogWrite "$msg`n" "red"
     $scriptname=$MyInvocation.ScriptName
     "Winlogcheck (or Logcheck for Windows)`n
     `Usage:`n
-    `$scriptname -mode ignore [ -log <eventlog_name> ]`n
+    `$scriptname -mode ignore`n
     `$scriptname -mode special -filter <filter_name>`n
     `$scriptname -mode test -filter <filter_name>`n`n"
 }
@@ -37,13 +37,13 @@ function makeEventQuery($log, $where) {
 
 function getEvents($log, $where) {
     $query = makeEventQuery $log $where
+    LogWrite("Run query: $query")
     [wmisearcher]$wmis = $query
     try {
         return $wmis.Get()
     } catch {
-        Write-Host "$msg : failed.`nQuery: $query`n" -foregroundcolor "red"
-        "Exception:`n"
-        $_.Exception.ToString()
+        LogWrite "$msg : failed."  "red"
+        LogWrite("Exception:`n" + $_.Exception.ToString())
         exit (1)
     }
 }
@@ -61,6 +61,7 @@ function createReport($events) {
 ### Task TEST ###
 
 function runTest($filter) {
+    LogWrite("Test filter: '$filter'")
     $ignoreFilterPath = ignoreFilterPath($filter)
     $specialFilterPath = specialFilterPath($filter)
     $where = ""
@@ -68,34 +69,27 @@ function runTest($filter) {
     if (Test-Path $ignoreFilterPath) {
         $where = Get-Content $ignoreFilterPath
         $filterin = "ignore"
+        LogWrite("Filter found: '$ignoreFilterPath'")
     }
     elseif (Test-Path $specialFilterPath) {
         $where = Get-Content $specialFilterPath
         $filterin = "special"
+        LogWrite("Filter found: '$specialFilterPath'")
     }
     else {
-        Write-Host "Filter '$filter' not found`n" -foregroundcolor "red"
+        LogWrite "Filter '$filter' not found" "red"
         exit(1)
     }
     $log =  $filter.Split('.')[0]
     $msg = "Test filter='$filter' in '$filterin' for log='$log'"
     if ( (Get-EventLog -List | Where-Object {$_.Log -eq $log} ).Length -eq 0) {
-        Write-Host "$msg : failed. Log '$log' not found`n" -foregroundcolor "red"
+        LogWrite "$msg : failed. Log '$log' not found`n" "red"
         exit (1)
         
     }
     $query = makeEventQuery $log $where
-    try {
-        (getEvents  $log $where).Lenght
-        Write-Host "$msg : OK`n" -foregroundcolor "green"
-        "Query:  $query"
-    } catch {
-        Write-Host "$msg : failed.`nQuery: $query`n" -foregroundcolor "red"
-        "Exception:`n"
-        $_.Exception.ToString()
-        exit (1)
-    }
-
+    (getEvents  $log $where).Lenght
+    LogWrite "$msg : OK" "green"
 }
 
 ### Task SPECIAL ###
@@ -106,7 +100,7 @@ function runSpecial($filter) {
         $where = Get-Content $specialFilterPath
     }
     else {
-        Write-Host "Special filter '$filter' not found`n" -foregroundcolor "red"
+        LogWrite "Special filter '$filter' not found`n" "red"
         exit(1)
     }
     $events = getEvents ($filter.Split('.')[0]) $where
@@ -117,7 +111,7 @@ function runSpecial($filter) {
 
 ### Task IGNORE ###
 
-function runIgnore($log) {
+function runIgnore() {
     foreach ($log in (Get-EventLog -List)) {
         $logname = $log.Log
         $filters = @()
@@ -138,19 +132,7 @@ function runIgnore($log) {
 # MAIN PROCEDURE
 #
 
-# 1a Step. Check command line
-if (!$mode -or  (($mode -ne "ignore") -and ($mode -ne "special") -and ($mode -ne "test"))) {
-    writeUsage("Error: Mode not set or not found")
-    exit(1)
-} 
-elseif ( ($mode -eq "special") -or ($mode -eq "test") ) {
-    if (!$filter) {
-        writeUsage("Error: Filter not set")
-        exit(1)
-    } 
-}
-
-# 1b Step. Read parameters (if exsist) from ini-file and set defaults
+# 1a Step. Read parameters (if exsist) from ini-file and set defaults
 $inifile = Join-Path $PSScriptRoot ( $MyInvocation.MyCommand.Name.Replace("ps1", "ini") )
 $ini = @{}
 if (Test-Path $inifile) {
@@ -169,28 +151,54 @@ if (!($ini.ContainsKey("DEPTHHOURS"))) {
     $ini.Add("DEPTHHOURS", 24) # One week
 }
 
-# 1c Step. Simple logging
+# 1b Step. Simple logging
 # Remove old logfiles (YYYYMMDD.log)
 Get-ChildItem $ini["LOGPATH"] | Where-Object {$_.Name -match "^\d{8}.log$"}`
     | ? {$_.PSIsContainer -eq $false -and $_.lastwritetime -lt (get-date).adddays(-$ini["LOGSTORETIME"])}`
     | Remove-Item -Force
 
 # Create logfile 
-$log = Join-Path $ini["LOGPATH"] ( (Get-Date).ToString('yyyyMMdd') + ".log" )
-if (!(Test-Path $log)) {
-    New-Item -type file $log -force | Out-Null
+$logfile = Join-Path $ini["LOGPATH"] ( (Get-Date).ToString('yyyyMMdd') + ".log" )
+if (!(Test-Path $logfile)) {
+    New-Item -type file $logfile -force | Out-Null
 }
 # and simple function for logging to screen/logfile
-function LogWrite($msg) {
-    Add-Content $log ((get-date -format HH:mm:ss) + " " + $msg)
-    Write-Output $msg
+function LogWrite($msg, $color) {
+    Add-Content $logfile ((get-date -format HH:mm:ss) + " " + $msg)
+    if ([bool]$color) {
+        Write-Host "$msg" -foregroundcolor $color
+    } 
+    else {
+        Write-Host $msg
+    }
+}
+
+Logwrite ("Start {0}`n
+Parameters:`n
+    `LOGPATH`t`t= {0}
+    `LOGSTORETIME`t= {1}
+    `RPTPATH`t`t= {2}
+    `DEPTHHOURS`t`t= {3}`n
+    " -f $MyInvocation.MyCommand.Name, 
+    $ini["LOGPATH"], $ini["LOGSTORETIME"], $ini["RPTPATH"], $ini["DEPTHHOURS"])
+
+# 1c Step. Check command line
+if (!$mode -or  (($mode -ne "ignore") -and ($mode -ne "special") -and ($mode -ne "test"))) {
+    writeUsage("Error: Mode not set or not found")
+    exit(1)
+} 
+elseif ( ($mode -eq "special") -or ($mode -eq "test") ) {
+    if (!$filter) {
+        writeUsage("Error: Filter not set")
+        exit(1)
+    } 
 }
 
 ####
 #### 2 MAIN STEP. Run Task ###
 ####
 switch ($mode) {
-    "ignore"  { runIgnore($log) }
+    "ignore"  { runIgnore }
     "special" { runSpecial($filter) }
     "test"    { runTest($filter) }
 }
